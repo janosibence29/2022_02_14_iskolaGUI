bbcf6cdb5fa5b139f12bd90b24722aa188ed2032
janosibence29 <92314900+janosibence29@users.noreply.github.com>
2022-02-14 13:45:55 +0100

    javitas / uj

--- 2022_02_14_iskolaGUI/Form1.cs
+++ 2022_02_14_iskolaGUI/Form1.cs
@@ -9,21 +9,34 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
 
-namespace _2022_02_14_iskolaGUI
+namespace _2022_02_14_Iskola
 {
-    public partial class Form1 : Form
+    public partial class IskolaGUI : Form
     {
-        public Form1()
+        public IskolaGUI()
         {
             InitializeComponent();
         }
 
-       
-
         private void Form1_Load(object sender, EventArgs e)
         {
             File.ReadAllLines("nevek.txt").ToList()
-                .ForEach(sor => lsitBox1.Items.Add(sor));
+                .ForEach(sor => listBox1.Items.Add(sor));
+        }
+
+        private void btnTörlés_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex == -1)
+            {
+                MessageBox.Show("Nem jelölt ki tanulót !!");
+            }
+            else
+            {
+                listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+            }
+
+
+
         }
     }
 }
--- iskolagui2/Form1.Designer.cs
+++ iskolagui2/Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
-namespace _2022_02_14_Iskola
+namespace iskolagui2
 {
-    partial class IskolaGUI
+    partial class Form1
     {
         /// <summary>
         /// Required designer variable.
@@ -30,49 +30,68 @@ namespace _2022_02_14_Iskola
         private void InitializeComponent()
         {
             this.listBox1 = new System.Windows.Forms.ListBox();
-            this.btnTörlés = new System.Windows.Forms.Button();
+            this.btnBetöltés = new System.Windows.Forms.Button();
             this.btnMentés = new System.Windows.Forms.Button();
+            this.btnTörlés = new System.Windows.Forms.Button();
+            this.btnKilépés = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(-1, 1);
+            this.listBox1.Location = new System.Drawing.Point(2, -1);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(166, 342);
+            this.listBox1.Size = new System.Drawing.Size(156, 342);
             this.listBox1.TabIndex = 0;
-            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
-            // btnTörlés
+            // btnBetöltés
             // 
-            this.btnTörlés.Location = new System.Drawing.Point(-1, 360);
-            this.btnTörlés.Name = "btnTörlés";
-            this.btnTörlés.Size = new System.Drawing.Size(75, 23);
-            this.btnTörlés.TabIndex = 1;
-            this.btnTörlés.Text = "Törlés";
-            this.btnTörlés.UseVisualStyleBackColor = true;
+            this.btnBetöltés.Location = new System.Drawing.Point(2, 348);
+            this.btnBetöltés.Name = "btnBetöltés";
+            this.btnBetöltés.Size = new System.Drawing.Size(75, 23);
+            this.btnBetöltés.TabIndex = 1;
+            this.btnBetöltés.Text = "Betöltés";
+            this.btnBetöltés.UseVisualStyleBackColor = true;
             // 
             // btnMentés
             // 
-            this.btnMentés.Location = new System.Drawing.Point(80, 360);
+            this.btnMentés.Location = new System.Drawing.Point(83, 348);
             this.btnMentés.Name = "btnMentés";
             this.btnMentés.Size = new System.Drawing.Size(75, 23);
             this.btnMentés.TabIndex = 2;
             this.btnMentés.Text = "Mentés";
             this.btnMentés.UseVisualStyleBackColor = true;
-            this.btnMentés.Click += new System.EventHandler(this.btnMentés_Click);
+            // 
+            // btnTörlés
+            // 
+            this.btnTörlés.Location = new System.Drawing.Point(2, 377);
+            this.btnTörlés.Name = "btnTörlés";
+            this.btnTörlés.Size = new System.Drawing.Size(75, 23);
+            this.btnTörlés.TabIndex = 3;
+            this.btnTörlés.Text = "Törlés";
+            this.btnTörlés.UseVisualStyleBackColor = true;
+            // 
+            // btnKilépés
+            // 
+            this.btnKilépés.Location = new System.Drawing.Point(83, 377);
+            this.btnKilépés.Name = "btnKilépés";
+            this.btnKilépés.Size = new System.Drawing.Size(75, 23);
+            this.btnKilépés.TabIndex = 4;
+            this.btnKilépés.Text = "Kilépés";
+            this.btnKilépés.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(172, 395);
-            this.Controls.Add(this.btnMentés);
+            this.ClientSize = new System.Drawing.Size(164, 405);
+            this.Controls.Add(this.btnKilépés);
             this.Controls.Add(this.btnTörlés);
+            this.Controls.Add(this.btnMentés);
+            this.Controls.Add(this.btnBetöltés);
             this.Controls.Add(this.listBox1);
             this.Name = "Form1";
-            this.Text = "IskolaGUI";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Text = "Form1";
             this.ResumeLayout(false);
 
         }
@@ -80,8 +99,10 @@ namespace _2022_02_14_Iskola
         #endregion
 
         private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.Button btnTörlés;
+        private System.Windows.Forms.Button btnBetöltés;
         private System.Windows.Forms.Button btnMentés;
+        private System.Windows.Forms.Button btnTörlés;
+        private System.Windows.Forms.Button btnKilépés;
     }
 }
 
